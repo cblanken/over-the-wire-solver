3e9c2bea41bcb9574af9c8971b613cabf11e10f3
Cameron Blankenbuehler <cameron.blankenbuehler@protonmail.com>
2022-03-29 21:41:40 -0400

    Refactor main function and add check to validate 'next_password'

--- solve.py
+++ solve.py
@@ -1,42 +1,63 @@
-from paramiko import SSHClient, AutoAddPolicy
+from paramiko import SSHClient, AutoAddPolicy, AuthenticationException, SSHException
 from os import path
 import json
+import sys
 
-def pwn_ssh(user, host, port, password, command_string, sshKeyPath = None):
+from paramiko.pkey import PKey
+
+def pwn_ssh(user, host, port, password, command_string, sshKeyPath=None):
     # TODO implement with pwntools
     print("Logging in with pwntools")
 
-def para_ssh(user, host, port, password, command_string, sshKeyPath = None):
-    c = SSHClient()
-    c.load_system_host_keys()
+def para_ssh(host, port, user, password, command_string, sshKeyPath=None):
+    client = SSHClient()
+    client.load_system_host_keys()
     print(f"\n============= {user} =============")
     try:
         if (sshKeyPath != None):
             print("Passing ssh key...")
         else:
             print(f"Logging into {user}...")
-            c.set_missing_host_key_policy(AutoAddPolicy())
-            c.connect(host, port, user, password, allow_agent=False, look_for_keys=False)
+            client.set_missing_host_key_policy(AutoAddPolicy())
+            client.connect(host, port, user, password, allow_agent=False, look_for_keys=False)
 
             print(f"Executing {user} commands...") 
-            stdin, stdout, stderr = c.exec_command(command_string)
+            stdin, stdout, stderr = client.exec_command(command_string)
 
             next_password = stdout.read().decode('utf8')
-            print(f"===== STDOUT =====\n{next_password}")
+            # print(f"===== STDOUT =====\n{next_password}")
                         
             stdin.close()
             stdout.close()
             stderr.close()
 
             print(f"Logging out of {user}...")
-            c.close()
+            client.close()
 
             # Return password for the next level without newline
-            return next_password[0:32]
+            return next_password[:-1]
     except Exception as e:
         print("paramiko failed")
         print(str(e))
 
+
+def test_ssh_login(host, port, user, password, key=None):
+    client = SSHClient()
+    client.load_system_host_keys()
+    try:
+        client.connect(host, port, user, password, pkey=key, allow_agent=False, look_for_keys=False)
+        return True
+    except AuthenticationException:
+        print(f"Authentication failure. Invalid password or private key. Cannot login as {user}.")
+        return None
+    except SSHException:
+        print(f"""SSH error. Cannot connect. The connection parameters were:\n
+                \thost: {host}\n
+                \tport: {port}\n
+                \tusername: {user}\n
+                \tpassword: {password}\n
+                \tprivate key (starting with): {key[0:20]}...""")
+        return None
 
 def parse_cfg(filepath):
     try:
@@ -46,21 +67,31 @@ def parse_cfg(filepath):
     except Exception as e:
         print(str(e))
 
+def main(config_path, bandit_levels):
+    next_password = "bandit0"
+    for i, level in enumerate(bandit_levels[:-1]):
+        cfg = parse_cfg(f"{config_path}/{level}.cfg")
+        cfg = {} if cfg is None else cfg
+        try:
+            username = cfg["user"]
+            password = next_password
+            host = cfg["host"]
+            port = cfg["port"]
+            commands = cfg["commands"]
+
+            command_string = "; ".join(commands)
+            next_password = para_ssh(host, port, username, password, command_string)
+            print(f"Password for {bandit_levels[i+1]}: {next_password}")
+            if test_ssh_login(host, port, bandit_levels[i+1], next_password):
+                print(f"{level} solved! The password for the {bandit_levels[i+1]} is correct!")
+        except KeyError:
+            print(f"Config file not found for {level}, continuing to next level.")
 
 if __name__ == "__main__":
-    config_path = path.abspath("config")
-    banditLevels = [f"bandit{x}" for x in range(0,6)]
+    if len(sys.argv) != 2:
+        print("Usage: python solve.py <max_level>")
+    else:
+        config_path = path.abspath("config")
+        bandit_levels = [f"bandit{x}" for x in range(0,int(sys.argv[1]) + 1)]
+        main(config_path, bandit_levels)
 
-    next_password = "bandit0"
-    for level in banditLevels:
-        cfg = parse_cfg(f"{config_path}/{level}.cfg")
-        username = cfg["user"]
-        # password = cfg["pass"]
-        password = next_password
-        host = cfg["host"]
-        port = cfg["port"]
-        commands = cfg["commands"]
-
-        command_string = "; ".join(commands)
-
-        next_password = para_ssh(username, host, port, password, command_string)
